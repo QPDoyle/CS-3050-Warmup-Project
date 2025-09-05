a7509780bbea35462c3131581bc72451181e7510
Bee Millie <bee.millie137@gmail.com>
2025-09-05 01:32:43 -0400

    Compound queries are accepted and errors are handled

--- query.py
+++ query.py
@@ -1,5 +1,5 @@
 import pyparsing as pp
-from pyparsing import rest_of_line
+from pyparsing import rest_of_line, ParseException
 
 test_string = "object_one == object_==two"
 first_keyword = pp.one_of("population region ocean borders")
@@ -13,12 +13,36 @@ while True:
         break
     
     elif raw_input.lower() == "help":
-        print("placeholder help text")
-    
-    else:
+        print("Please follow the format of 'keyword operator value'\n"
+              "Keywords: population, region, ocean, borders\n"
+              "Operators: ==, <, >, of\n"
+              "Value: state names, number values\n"
+              "Examples: population of New Mexico, borders > 3")
 
-        parse_format = first_keyword + operators + rest_of_line
-        parsed_string = parse_format.parse_string(raw_input)
-        print(parsed_string)
+    elif ' and ' in raw_input:
+        try:
+            parse_format = (first_keyword + operators + second_keyword + "and" +
+                            first_keyword + operators + second_keyword)
+            parsed_string = parse_format.parse_string(raw_input)
+            print(parsed_string)
+        except ParseException as e:
+            print("This is not a valid query. Please try again")
+
+    elif ' or ' in raw_input:
+        try:
+            parse_format = (first_keyword + operators + second_keyword + "or" +
+                            first_keyword + operators + second_keyword)
+            parsed_string = parse_format.parse_string(raw_input)
+            print(parsed_string)
+        except ParseException as e:
+            print("This is not a valid query. Please try again")
+
+    else:
+        try:
+            parse_format = first_keyword + operators + rest_of_line
+            parsed_string = parse_format.parse_string(raw_input)
+            print(parsed_string)
+        except ParseException as e:
+            print("This is not a valid query. Please try again")
     
     
