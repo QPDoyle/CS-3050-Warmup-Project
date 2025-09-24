4c81e72c360795be2e488620e809508652765d4c
HaileyEmeline <hschoppe@uvm.edu>
2025-09-24 17:23:10 -0400

    Added comments to admin and firestore files

--- admin.py
+++ admin.py
@@ -1,10 +1,12 @@
 from firestore import firebase_auth
 import json
 
-#State class - per documentation 
-#https://firebase.google.com/docs/firestore/query-data/get-data
+#State class with various attributes
+#Firestore documentation: https://firebase.google.com/docs/firestore/query-data/get-data
 class State:
     def __init__(self, name, abbreviation, region, population, borders, ocean):
+
+        #Initialize state object with its attributes
         self.name = name
         self.abbreviation = abbreviation
         self.region = region
@@ -12,34 +14,42 @@ class State:
         self.borders = borders
         self.ocean = ocean
     
+    #Creates a state object from a dictionary 
     @staticmethod
     def from_dict(source):
 
-        #Converts oceans to a list
+        #Converts oceans to a list by splitting the string with commas
         ocean = source.get("ocean")
         if ocean:
             list_of_oceans = ocean.split(",")
         else:
+            #If there are no oceans, return an empty list
             list_of_oceans = []
 
+        #Returns a state object constructed from dictionary data
         return State(
+
             name = source.get("name"),
             abbreviation = source.get("abbreviation"),
             region = source.get("region"),
             population = source.get("population"),
             borders = source.get("borders"),
             ocean = list_of_oceans
+
         )
     
+    #Puts states into dictionary data
     def to_dict(self):
 
+        #Converts oceans to a list by splitting the string with commas
         ocean = self.ocean
         if ocean:
             list_of_oceans = ocean.split(",")
         else:
+            #If there are no oceans, return an empty list
             list_of_oceans = []
         
-
+        #Return the state object as a dictionary representation
         return {
             "name": self.name,
             "abbreviation": self.abbreviation,
@@ -49,8 +59,9 @@ class State:
             "ocean": list_of_oceans
         }
     
-    #From what exists already in the documentation
+    #String representation of the state objects
     def __repr__(self):
+
         return f"State(\
             name = {self.name}, \
             abbreviation = {self.abbreviation}, \
@@ -61,23 +72,23 @@ class State:
             )"
 
 
+#Function uploads the states from a JSON file into Firestore
 def uploadJSON():
 
-    #db is the database
+    #Reference the database as db
     db = firebase_auth()
     
-    #open json file
+    #Open json file with state data
     with open('states.json', 'r') as file:
         data = json.load(file)
 
-    #Creates a collection called states
+    #Creates/accesses a collection called states
     states_ref = db.collection("States")
 
 
     for state in data['states']:
 
         #Creates a document with the name of the state
-        #.Set is what pushes it to firebase - sets the data inside the parentheses to the document listed
         states_ref.document(state['name']).set(
 
             #Creates a State object, pulls the data for it out of the JSON
--- firestore.py
+++ firestore.py
@@ -2,15 +2,17 @@ import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
 
+#Function connects and authorizes the user to Firebase Firestore using JSON authentication file
 def firebase_auth():
 
     #update below to say r"\path\to\file.json"
     if (not firebase_admin._apps):
-        cred = credentials.Certificate(r"C:\Users\darkf\Downloads\cs3050-warmup-baf96-firebase-adminsdk-fbsvc-470d196a80.json")
+        cred = credentials.Certificate(r"INSERT PATH TO JSON FILE")
         firebase_admin.initialize_app(cred)
 
     db = firestore.client()
 
+    #Returns the client connection to the database
     return db
 
 
